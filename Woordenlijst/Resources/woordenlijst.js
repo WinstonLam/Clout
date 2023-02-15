67e8ba4e659c7e66f291600ccaee54820bc17903
Douwe <schepersdouwe@gmail.com>
2023-02-15 11:26:55 +0100

    server setup

---
+++ Woordenlijst/Resources/woordenlijst.js
@@ -0,0 +1,17 @@
+function get()
+{
+    return "get woordenlijst";
+}
+function post()
+{
+    return "post woordenlijst ";
+}
+function getById()
+{
+    return "get by id";
+}
+function deleteById()
+{
+    return "delete";
+}
+module.exports = { get, post, getById, deleteById}
