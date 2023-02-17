705d91d7a3aec4d0065a76de0b40b575f07b0d45
Winston Lam <winston.lam@outlook.com>
2023-02-17 11:31:43 +0100

    rearranged files

--- winston/client/src/components/word-input.js
+++ winston/client/src/components/word-input.js
@@ -1,29 +1,29 @@
-import React, { useState } from "react";
+import React from "react";
 import "../styles/word-input.css";
 import { TextField } from "@mui/material";
 
-export default function WordInput() {
-  const [values, setValues] = useState({
-    word: "",
-    definition: "",
-  });
-
+export default function WordInput({
+  onChangeWord,
+  onChangeDefinition,
+  word,
+  definition,
+}) {
   return (
     <div className="word-input-line">
       <TextField
         id="word-input"
         label="Type the word"
-        value={values.word}
+        value={word}
         variant="outlined"
-        onChange={(e) => setValues({ ...values, word: e.target.value })}
+        onChange={onChangeWord}
         style={{ width: "30%" }}
       />
       <TextField
         id="word-input"
         label="Type the definition"
-        value={values.definition}
+        value={definition}
         variant="outlined"
-        onChange={(e) => setValues({ ...values, definition: e.target.value })}
+        onChange={onChangeDefinition}
         style={{ width: "50%" }}
       />
     </div>
