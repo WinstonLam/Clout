64e5a08b96e1b50e928fa436cae2b734ec204a82
Winston Lam <winston.lam@outlook.com>
2023-02-26 13:18:39 +0100

    Added backend communication to wordlist component

--- winston/client/src/App.js
+++ winston/client/src/App.js
@@ -1,32 +1,31 @@
-import { useEffect } from "react";
 import Wordlist from "./components/Wordlist";
-import { wordlist, word } from "./proto/client_pb";
 import { UserServiceClient } from "./proto/ClientServiceClientPb.ts";
 
 function App() {
-  useEffect(() => {
-    (async () => {
-      const client = new UserServiceClient("http://localhost:8080");
-      const woord = new word();
-      const list = new wordlist();
-      woord.setWord("maan");
-      woord.setDescription("boven de aarde");
+  const client = new UserServiceClient("http://localhost:8080");
+  // useEffect(() => {
+  //   (async () => {
+  //     const client = new UserServiceClient("http://localhost:8080");
+  //     const woord = new word();
+  //     const list = new wordlist();
+  //     woord.setWord("maan");
+  //     woord.setDescription("boven de aarde");
 
-      list.setWordlistname("test1");
-      list.setDescription("test");
-      list.setWordsList([woord]);
+  //     list.setWordlistname("test1");
+  //     list.setDescription("test");
+  //     list.setWordsList([woord]);
 
-      client.addNewWordlist(list, {}, (err, response) => {
-        if (err) console.log(err.metadata);
-        else console.log(response);
-      });
-    })();
-  }, []);
+  //     client.addNewWordlist(list, {}, (err, response) => {
+  //       if (err) console.log(err.metadata);
+  //       else console.log(response);
+  //     });
+  //   })();
+  // }, []);
 
   return (
     <div className="App">
       <header className="App-header"></header>
-      <Wordlist />
+      <Wordlist client={client} />
     </div>
   );
 }
--- winston/client/src/components/wordlist-buttons.js
+++ winston/client/src/components/wordlist-buttons.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Button } from "@mui/material";
 import "../styles/wordlist-buttons.css";
 
-export default function WordListButtons({ createWordlist }) {
+export default function WordListButtons({ onSubmit }) {
   return (
     <div className="wordlist-buttons-wrapper">
       <div className="wordlist-buttons">
         <Button variant="contained">Back</Button>
-        <Button variant="contained" onClick={createWordlist}>
+        <Button variant="contained" onClick={onSubmit}>
           Create
         </Button>
       </div>
